edef8091afd1f1fb06c4ba92e0a24f64b1b58e70
Juan Gonzalez-Miret <jgonalv3009@g.educaand.es>
2023-11-13 17:36:24 +0100

    primer commit sin archivo modif

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo "Hola mundo soy Juan Gonzalez-Miret y estoy estudiando ciber";
 echo "Estoy modificando index.php desde github.";
+echo "Ahora son cambios desde la termial. Nueva modficacion";
 ?>
