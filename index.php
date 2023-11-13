e203390615076dcf86bb397880ae203d5ee10e5d
Juan Gonzalez-Miret <jgonalv3009@g.educaand.es>
2023-11-13 17:43:35 +0100

    Eliminado index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Hola mundo soy Juan Gonzalez-Miret y estoy estudiando ciber";
-echo "Estoy modificando index.php desde github.";
-echo "Ahora son cambios desde la termial. Nueva modficacion";
-?>
